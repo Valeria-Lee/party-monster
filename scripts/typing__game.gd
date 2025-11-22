d2c508c059f7e629153fce485ac579ab11e837ab
Miguel Alejandro <migtorruco@gmail.com>
2025-11-22 00:17:56 -0600

    feat: timer added without logic yet

--- scripts/typing__game.gd
+++ scripts/typing__game.gd
@@ -2,6 +2,8 @@ extends Control
 
 @onready var target_text = $CanvasLayer/UIRoot/TargetTextContainer/TargetText
 @onready var text_edit = $CanvasLayer/UIRoot/TextEditContainer/TextEdit
+@onready var timer_label = $CanvasLayer/UIRoot/TimerLabel
+@onready var timer = $Timer
 
 var text = "Computer science isn't about computers, much like astronomy isn't about telescopes. The computer was born to solve problems that did not exist before. " \
   			+ "One machine can do the work of fifty ordinary men. No machine can do the work of one extraordinary man"
@@ -13,4 +15,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if not timer.is_stopped():
+		var time = int(timer.time_left)
+		var minutes = time / 60
+		var seconds = time % 60
+		timer_label.text = "%02d:%02d" % [minutes, seconds]
