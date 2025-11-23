2150000b2a4ac52bf972f561a5c968bfd51c7318
Valeria-Lee <leev6893a@gmail.com>
2025-11-23 13:30:21 -0600

    coneccion de supermercado con minijuego lista, star npc en proceso...

--- singleton/manager.gd
+++ singleton/manager.gd
@@ -6,13 +6,19 @@ var dialogues: Dialogues
 var advice: String
 @onready var chef_dialogues = load("res://dialogue/chef_dialogues.tres")
 @onready var bakery = load("res://scenes/bakery.tscn")
+@onready var star_dialogues = load("res://dialogue/star_dialogues.tres")
+@onready var supermarket = load("res://scenes/supermarket.tscn")
 var bakery_pc_fixed: bool
+var supermarket_fixed: bool
 
 func init():
 	match current_character:
 		"chef":
 			dialogues = chef_dialogues
 			current_scene = bakery
+		"star":
+			dialogues = star_dialogues
+			current_scene = supermarket
 			
 func get_next_dialogue() -> String:
 	return dialogues.dialogues[dialogues.current_index]
