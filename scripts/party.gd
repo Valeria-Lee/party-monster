155d9aae016148ebd7296ca4e1ecba09f9678c29
Valeria-Lee <leev6893a@gmail.com>
2025-11-23 09:05:34 -0600

    juego fiesta detecta input, verificacion de palabras en proceso...

--- scripts/party.gd
+++ scripts/party.gd
@@ -5,41 +5,42 @@ extends Node3D
 var party_time_text = "[right][rainbow]Party protocol activated!\nThe countdown to maximum fun has begun: "
 @onready var word_spawner = $UI/WordSpawner
 @onready var word_label = $UI/WordSpawner/WordLabel
-var attack_words = ["AWKWARD DANCE MOVE","TICKLES","MERGE CONFLICT MAYHEM","POWER OF FRIENDSHIP","INFINITE LOOP KICK"]
+var attack_words = ["AWKWARD-DANCE-MOVE","TICKLES","MERGE-CONFLICT-MAYHEM","POWER-OF-FRIENDSHIP","INFINITE-LOOP-KICK"]
+@onready var rng = RandomNumberGenerator.new()
 var play_combat: bool = false
 var win: bool = false
 var rounds_won: int = 0
+var current_word
+var selected_word: String = ""
+var playing: bool = false
 
 func _ready() -> void:
 	add_child(timer)
-	timer.wait_time = 10.0 # subir a 20
+	timer.wait_time = 5.0 # subir a 20
 	timer.one_shot = true
 	timer.timeout.connect(_on_timer_timeout)
 	timer.start()
-	
-	var rng = RandomNumberGenerator.new()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if not timer.is_stopped():
 		var time = int(timer.time_left)
 		var seconds = time % 60
 		party_countdown.text = party_time_text + " " + str(seconds)
-	else:
-		if play_combat and rounds_won < 3:
-			pass
-			
 
 func _on_timer_timeout():
 	party_countdown.visible = false
 	
 	# aqui viene el policia a hablar contigo hija
+	
 	# esta es la pelea hija
 	play_combat = true
+	play_game()
 	
 func generate_random_word():
-	var selected_word = attack_words.pick_random()
+	selected_word = attack_words.pick_random()
+	print(selected_word)
 	word_label.text = selected_word
-	attack_words.erase(selected_word)
+	current_word = selected_word.split("")
 	print(attack_words)
 	
 func change_word_position():
@@ -55,7 +56,26 @@ func _input(event):
 		if event.pressed:
 			var key_name = OS.get_keycode_string(event.keycode)
 			print(key_name)
+			
+			if play_combat:
+				if current_word.is_empty():
+					return
+				
+				var typed_char = char(event.unicode)
+				
+				if key_name == "Minus":
+					typed_char = "-"
+					
+				if typed_char == current_word[0]:
+					current_word.pop_front()
+					print(current_word)
+				
+				if len(current_word) == 0:
+					rounds_won += 1
+					attack_words.erase(selected_word)
+					play_game()
 
 func play_game():
-	generate_random_word()
-	change_word_position()
+	if play_combat and rounds_won < 3:
+		generate_random_word()
+		change_word_position()
