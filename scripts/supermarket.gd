01df0c20f59ba603c7325c1c40c87870632fe7d9
Miguel Alejandro <migtorruco@gmail.com>
2025-11-23 21:21:34 -0600

    fix: door visibility

--- scripts/supermarket.gd
+++ scripts/supermarket.gd
@@ -4,12 +4,12 @@ extends Node3D
 @onready var party_scene = load("res://scenes/party.tscn")
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+#func _ready() -> void:
+	#door.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Manager.bakery_pc_fixed == true:
+	if Manager.supermarket_fixed == true:
 		door.visible = true
 
 func _on_door_body_entered(body: Node3D) -> void:
