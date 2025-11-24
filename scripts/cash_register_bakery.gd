a01c7f594581a861319ff9dad5ea128bb719a636
Miguel Alejandro <migtorruco@gmail.com>
2025-11-24 01:39:39 -0600

    feat: bakery cash register working

--- scripts/cash_register_bakery.gd
+++ scripts/cash_register_bakery.gd
@@ -1,11 +1,20 @@
 extends StaticBody3D
 
+var player = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("e") and player != null:
+		player.pay()
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.name == "Player":
+		player = body
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.name == "Player":
+		player = null
